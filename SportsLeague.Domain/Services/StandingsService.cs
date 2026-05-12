610517c64ebdb48e560a7abae5f823dac235b777
Carlos Díaz <carlos.diaz@itsystemsinnova.com>
2026-05-11 23:13:46 -0500

    More data added in DataSeeder

--- SportsLeague.Domain/Services/StandingsService.cs
+++ SportsLeague.Domain/Services/StandingsService.cs
@@ -167,11 +167,11 @@ namespace SportsLeague.Domain.Services
                 .GroupBy(g => new { g.PlayerId, g.Player.FirstName, g.Player.LastName, g.Player.TeamId })
                 .Select(group => new
                 {
-                    PlayerId = group.Key.PlayerId,
+                    group.Key.PlayerId,
                     PlayerName = group.Key.FirstName + " " + group.Key.LastName,
                     TeamName = group.First().Player.Team?.Name ?? "N/A",
                     Goals = group.Count(),
-                    Penalties = group.Count(g => g.Type == Enums.GoalType.Penalty),
+                    Penalties = group.Count(g => g.Type == GoalType.Penalty),
                     MatchesWithGoals = group.Select(g => g.MatchId).Distinct().Count()
                 })
                 .OrderByDescending(s => s.Goals)
@@ -204,11 +204,11 @@ namespace SportsLeague.Domain.Services
                 .GroupBy(c => new { c.PlayerId, c.Player.FirstName, c.Player.LastName })
                 .Select(group => new
                 {
-                    PlayerId = group.Key.PlayerId,
+                    group.Key.PlayerId,
                     PlayerName = group.Key.FirstName + " " + group.Key.LastName,
                     TeamName = group.First().Player.Team?.Name ?? "N/A",
-                    YellowCards = group.Count(c => c.Type == Enums.CardType.Yellow),
-                    RedCards = group.Count(c => c.Type == Enums.CardType.Red),
+                    YellowCards = group.Count(c => c.Type == CardType.Yellow),
+                    RedCards = group.Count(c => c.Type == CardType.Red),
                     TotalCards = group.Count()
                 })
                 .OrderByDescending(s => s.RedCards)
